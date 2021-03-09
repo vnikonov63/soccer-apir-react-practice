ee1d5d94e709b2f2f8b60289209cf9f948a28ea1
vnikonov63 <nikonov.va@yandex.ru>
2021-03-09 21:59:59 +0400

    managed to learn how to find the administrative_vlevel_1 location within the map

--- app/src/GoogleMapsAPIPractice/MarkerInfo/index.js
+++ app/src/GoogleMapsAPIPractice/MarkerInfo/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import styled from "styled-components";
 
@@ -12,12 +13,38 @@ const MarkerInfoBoundary = styled.div`
 `;
 
 function MarkerInfo({ positionInfo, index }) {
+  const [addressFind, setAddressFind] = useState(false);
+  const [humanAdress, setHumanAdress] = useState({});
+
+  useEffect(() => {
+    async function fetchReverseGeolocation() {
+      const apiAdress = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${positionInfo.lat},${positionInfo.lng}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+      const response = await axios.get(apiAdress);
+      const a = response.data.results.find((adress) => {
+        return adress.types.includes("administrative_area_level_1");
+      });
+      setHumanAdress(a.formatted_address);
+    }
+    fetchReverseGeolocation();
+  }, []);
+
   return (
     <MarkerInfoBoundary>
       <h2>Marker Number: {index}</h2>
       <h3>Lattitude: {`${positionInfo.lat}`.substr(0, 7)}</h3>
       <h3>Longitude: {`${positionInfo.lng}`.substr(0, 7)}</h3>
       <h3>Time pressed: {positionInfo.time.getTime()}</h3>
+      {addressFind ? (
+        <p>{humanAdress}</p>
+      ) : (
+        <button
+          onClick={() => {
+            setAddressFind(true);
+          }}
+        >
+          Find closest adress
+        </button>
+      )}
     </MarkerInfoBoundary>
   );
 }
