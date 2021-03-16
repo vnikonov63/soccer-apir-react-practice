b4ec0eca5ba8aae8d807876791e576d0f1b4a84d
vnikonov63 <nikonov.va@yandex.ru>
2021-03-17 03:47:07 +0400

    new frontend MVP nearly done

--- app/src/GoogleMapsAPIPractice/ComparissonTeams.js
+++ app/src/GoogleMapsAPIPractice/ComparissonTeams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { set } from "mongoose";
 
 export default function ComparissonTeams({ teams }) {
   const [games, setGames] = useState([]);
@@ -38,6 +39,29 @@ export default function ComparissonTeams({ teams }) {
             });
           }
         });
+
+        information.map((game) => {
+          const newGame = {
+            localTeam:
+              game.localteam_id === team1Id
+                ? teams[0].TeamName
+                : teams[1].TeamName,
+            localTeamLogo:
+              game.localteam_id === team1Id ? teams[0].Logo : teams[1].Logo,
+            visitorTeamLogo:
+              game.localteam_id === team1Id ? teams[1].Logo : teams[0].Logo,
+            visitorTeam:
+              game.localteam_id === team1Id
+                ? teams[1].TeamName
+                : teams[0].TeamName,
+            localTeamScore: game.scores.localteam_score,
+            visitorTeamScore: game.scores.visitorteam_score,
+            date: game.time.starting_at.date,
+          };
+          setGames((prev) => {
+            return [...prev, newGame];
+          });
+        });
         console.log(teams[0].TeamName, team1Stats);
         console.log(teams[1].TeamName, team2Stats);
       } catch (error) {
@@ -48,35 +72,57 @@ export default function ComparissonTeams({ teams }) {
     getTeamsInfo();
   }, []);
   return (
-    <div>
+    <>
+      <div>
+        <button className="btn red">Reset</button>
+      </div>
       <div
         style={{
           display: "flex",
           flexDirection: "column",
+          justifyContent: "space-around",
           alignItems: "center",
         }}
       >
-        <h3>{teams[0].TeamName}</h3>
-        <img
-          alt={"Team Logo"}
-          src={teams[0].Logo}
-          style={{
-            width: "200px",
-            height: "200px",
-          }}
-        />
         <div
           style={{
             display: "flex",
+            justifyContent: "space-around",
           }}
         >
-          {team1Stats.length !== 0 &&
-            team1Stats.map((result) => {
-              return <CircleComponent color={result} />;
-            })}
+          {team1Stats.length !== 0 && (
+            <DisplayTeam
+              teamLogo={teams[0].Logo}
+              teamStats={team1Stats}
+              teamName={teams[0].TeamName}
+            />
+          )}
+          <div style={{ width: "100px" }}></div>
+          {team2Stats.length !== 0 && (
+            <DisplayTeam
+              teamLogo={teams[1].Logo}
+              teamStats={team2Stats}
+              teamName={teams[1].TeamName}
+            />
+          )}
         </div>
+        <h2>List of Previous Games</h2>
+        {games.length !== 0 &&
+          games.map((game) => {
+            return (
+              <Game
+                localTeam={game.localTeam}
+                visitorTeam={game.visitorTeam}
+                localTeamScore={game.localTeamScore}
+                visitorTeamScore={game.visitorTeamScore}
+                localTeamLogo={game.localTeamLogo}
+                visitorTeamLogo={game.visitorTeamLogo}
+                date={game.date}
+              />
+            );
+          })}
       </div>
-    </div>
+    </>
   );
 }
 
@@ -92,5 +138,69 @@ function CircleComponent({ color }) {
           color === "win" ? "green" : color === "loss" ? "red" : "yellow",
       }}
     ></div>
+  );
+}
+
+function DisplayTeam({ teamLogo, teamStats, teamName }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h3>{teamName}</h3>
+      <img
+        alt={"Team Logo"}
+        src={teamLogo}
+        style={{
+          width: "200px",
+          height: "200px",
+        }}
+      />
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        {teamStats.map((result) => {
+          return <CircleComponent color={result} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+function Game({
+  localTeam,
+  visitorTeam,
+  localTeamScore,
+  visitorTeamScore,
+  localTeamLogo,
+  visitorTeamLogo,
+  date,
+}) {
+  return (
+    <div style={{ display: "flex", margin: "20px" }}>
+      <div style={{ display: "flex" }}>
+        <img
+          style={{ width: "100px", height: "100px", marginRight: "100px" }}
+          alt={"local team logo"}
+          src={localTeamLogo}
+        />
+        <h3>{localTeamScore}</h3>
+      </div>
+      <div style={{ width: "75px" }}></div>
+      <div style={{ display: "flex" }}>
+        <h3>{visitorTeamScore}</h3>
+        <img
+          style={{ width: "100px", height: "100px", marginLeft: "100px" }}
+          alt={"visitor team logo"}
+          src={visitorTeamLogo}
+        />
+      </div>
+      <h3 style={{ marginLeft: "75px" }}>{date}</h3>
+    </div>
   );
 }
